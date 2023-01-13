e6017df540bc7d2e1bb952d99b05d4a9d9ed042c
Ryan Taylor <ryantaylor50@hotmail.com>
2023-01-13 16:19:49 +0000

    edited questions and logic

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,71 +1,33 @@
-// creating an array and passing the number, questions, options, and answers
-let questions = [
+// List of Questions and Answers
+
+var questions = [
     {
-    numb: 1,
-    question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
-    options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
-    ]
-  },
+        prompt: "Inside which HTML element do we put the JavaScript?",
+        options: ["<javascript>", "<js>", "<script>", "<scripting>"],
+        answer: "<script>"
+    },
+
     {
-    numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
-    options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
-    ]
-  },
+        prompt: "How do you call a function named myFunction?",
+        options: ["call myFunction()", "myFunction()", "call function myFunction", "Call.myFunction"],
+        answer: "myFunction()"
+    },
+
     {
-    numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
-    options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
-    ]
-  },
+        prompt: "How does a for loop start?",
+        options: ["for (i = 0; i <= 5; i++)", "for (i = 0; i <= 5)", "for i = 1 to 5", " for (i <= 5; i++)"],
+        answer: "for (i = 0; i <= 5; i++)"
+    },
+
     {
-    numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
-    options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
-    ]
-  },
+        prompt: "In JavaScript, which of the following is a logical operator?",
+        options: ["|", "&&", "%", "/"],
+        answer: "&&" 
+    },
+
     {
-    numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
-    options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
-    ]
-  },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
-];
+        prompt: "A named element in a JavaScript program that is used to store and retrieve data is a _____.",
+        options: ["method", "assignment operator", "variable", "string"],
+        answer: "variable"
+    }];
+
